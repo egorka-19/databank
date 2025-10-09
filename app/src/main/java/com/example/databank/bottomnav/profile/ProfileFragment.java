3f657398c45186735f3b15ed3b5c3ec25f112839
Alexandr <sanchesterus6@gmail.com>
2025-10-10 01:36:45 +0400

    init profile design

--- app/src/main/java/com/example/databank/bottomnav/profile/ProfileFragment.java
+++ app/src/main/java/com/example/databank/bottomnav/profile/ProfileFragment.java
@@ -23,8 +23,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.bumptech.glide.Glide;
 import com.example.databank.Prevalent.Prevalent;
 import com.example.databank.R;
-import com.example.databank.UI.Users.favourite;
-import com.example.databank.UI.Users.settings;
+import com.example.databank.UI.Users.MainActivity;
 import com.example.databank.databinding.FragmentProfileBinding;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DataSnapshot;
@@ -78,19 +77,18 @@ public class ProfileFragment extends Fragment {
             }
         });
 
-        binding.buttonLike.setOnClickListener(new View.OnClickListener() {
+        binding.logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(ProfileFragment.this.getActivity(), favourite.class));
+                if (getActivity() == null) return;
+                Intent intent = new Intent(getActivity(), MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+                getActivity().finish();
             }
         });
 
-        binding.setting.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(ProfileFragment.this.getActivity(), settings.class));
-            }
-        });
+
 
         return binding.getRoot();
     }
